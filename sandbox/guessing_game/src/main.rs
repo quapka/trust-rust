db0e9bbf8e339e54db11857450fd16d38a998150
quapka <quapka@gmail.com>
2020-10-19 21:39:56 +0200

    Add guessing_game example

--- sandbox/guessing_game/src/main.rs
+++ sandbox/guessing_game/src/main.rs
@@ -1,3 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the secret number!");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
+
+    println!("Please input your guess: ");
+
+    loop {
+        print!("Please input your guess: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Warning: the input must be a number");
+                continue;
+            }
+        };
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
