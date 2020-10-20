f96b30d7aca8acc6b6ee642f0a8c31f27983223b
quapka <quapka@gmail.com>
2020-10-20 14:32:02 +0200

    Add test for secret number generation

--- sandbox/guessing_game/src/main.rs
+++ sandbox/guessing_game/src/main.rs
@@ -2,12 +2,25 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+fn generate_secret_number() -> u32 {
+    rand::thread_rng().gen_range(1, 101)
+}
+
+#[test]
+fn test_generate_secret_number_range() {
+    for _ in [0..1000].iter() {
+        let number: u32 = generate_secret_number();
+        assert!(1 <= number);
+        assert!(number <= 100);
+    }
+}
+
 fn main() {
     println!("Guess the secret number!");
 
-    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
-
     println!("Please input your guess: ");
+
+    let secret_number = generate_secret_number();
 
     loop {
         print!("Please input your guess: ");
