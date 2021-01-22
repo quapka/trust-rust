3c81f16f1df8ed5bd9c0b2e06d8fa2047bdbdd48
quapka <quapka@gmail.com>
2021-01-22 16:17:05 +0100

    Write results to a file

--- sandbox/xor/src/main.rs
+++ sandbox/xor/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
+use std::io::{BufReader, BufWriter};
 
 use structopt::StructOpt;
 
@@ -10,6 +10,8 @@ struct CliOptions {
     chunk_size: Option<usize>,
     #[structopt(parse(from_os_str))]
     input: std::path::PathBuf,
+    #[structopt(parse(from_os_str), short = "o", long = "output")]
+    output: Option<std::path::PathBuf>,
     #[structopt(short = "k", long = "key")]
     // FIXME what about non-ascii characters?
     // FIXME what about raw bytes: 0x80 0x10 or 0x801230
@@ -24,9 +26,21 @@ fn main() -> std::io::Result<()> {
         None => 1024,
     };
 
-    let file = File::open(args.input)?;
+    let file = File::open(&args.input)?;
 
     let mut reader = BufReader::with_capacity(chunk_size, file);
+    let out_file: std::path::PathBuf = match args.output {
+        Some(path) => path,
+        None => {
+            std::path::PathBuf::from([args.input.to_str().unwrap(), args.key.as_str()].join("-"))
+        }
+    };
+    let mut writer = BufWriter::new(File::create(out_file)?);
+
+    // buffer.write_all(b"some bytes")?;
+    // buffer.flush()?;
+
+    let mut keystream = args.key.bytes().cycle();
 
     loop {
         let buffer = reader.fill_buf()?;
@@ -35,17 +49,17 @@ fn main() -> std::io::Result<()> {
         }
         // let xored: [u8; chunk_size] = buffer.into_iter().map(|x| x ^ 13).collect();
         // let xored: Vec<u8> = buffer.into_iter().map(|x| x ^ 13).collect();
-        let xored: Vec<u8> = buffer
-            .iter()
-            .zip(args.key.bytes().cycle().take(chunk_size))
-            .map(|(p, k)| p ^ k)
-            .collect();
+        let chunk_key = keystream.by_ref().take(buffer.len());
+        let xored: Vec<u8> = buffer.iter().zip(chunk_key).map(|(p, k)| p ^ k).collect();
+        writer.write_all(&xored)?;
+        writer.flush()?;
 
-        println!("Read: {:?}", buffer);
-        println!("Xored: {:?}", xored);
+        // println!("Read: {:?}", buffer);
+        // println!("Xored: {:?}", xored);
         let n = buffer.len();
         reader.consume(n);
     }
+
     Ok(())
 }
 
