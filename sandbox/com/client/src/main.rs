35fad808653e7650d052a7b4298452b3137d8552
quapka <quapka@gmail.com>
2021-01-26 10:19:29 +0100

    Add the client side

---
+++ sandbox/com/client/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+fn main() {
+    println!("client");
+
+    let stdin = io::stdin();
+    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:65432") {
+        println!("connected");
+        for line in stdin.lock().lines() {
+            stream.write(line.unwrap().as_bytes());
+        }
+    } else {
+        println!("connection failed");
+    }
+}
